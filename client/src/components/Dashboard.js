f7552c6371cf33adfc00415c4a7f0831eed8f47a
krisszcode <kicsikrissz111@gmail.com>
2021-02-10 12:10:41 +0100

    initial commit

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SideBar from './SideBar'
+
+export default function Dashboard( {id} ) {
+  return (
+    <div className="d-flex" style={{height: '100vh'}}>
+      <SideBar id = {id}/>
+    </div>
+  )
+}
