9f949cf1b3976d40772bc2c98015eb5e11b1cf8e
krisszcode <kicsikrissz111@gmail.com>
2021-02-16 11:54:07 +0100

    users can send messages to each other

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useConversations } from '../contexts/ConversationsProvider'
+import OpenConversation from './OpenConversation'
 import SideBar from './SideBar'
 
 export default function Dashboard( {id} ) {
+  const {selectedConversation} = useConversations()
+
   return (
     <div className="d-flex" style={{height: '100vh'}}>
       <SideBar id = {id}/>
+      {selectedConversation && <OpenConversation/>}
     </div>
   )
 }
